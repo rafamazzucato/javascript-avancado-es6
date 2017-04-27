1d19e575d8294de5bd5f82b1661197a1effd83ec
rafamazzucato <rafael.thomazelli@gmail.com>
2017-04-27 20:02:54 -0300

    Inicio Controller
    inicio do desenvolvimento da camada de controle

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,8 +1,8 @@
 class Negociacao {
 
-    constructor(quantidade, valor) {
+    constructor(data, quantidade, valor) {
 
-        this._data = new Date();
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
@@ -14,7 +14,7 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
